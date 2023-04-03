d12d40f571f4e9b3d635065b1aace3c428a370f8
dohi0512 <donghuni512@gmail.com>
2023-04-03 20:23:10 +0900

    Add::소개 섹션, 카테고리 섹션

--- src/pageContainer/MainPage.tsx
+++ src/pageContainer/MainPage.tsx
@@ -1,3 +1,20 @@
-const MainPage = () => {
-    return <div>HI</div>
-}
+export const MainPage = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center pt-[10rem]">
+      <div className="w-[90%]">
+        <div className="w-full h-[14rem] rounded-[2rem] bg-BlueLight-20"></div>
+        <div className="w-full h-[2.5rem] flex justify-between mt-8">
+          <div className="grid grid-cols-3 h-full w-[45%] gap-8" >
+            <div className="category-items">카테고리</div>
+            <div className="category-items">카테고리1</div>
+            <div className="category-items">카테고리2</div>
+          </div>
+          <div className="w-[8rem] category-items text-GreenLight-30">파티 만들기</div>
+        </div>
+        <div className="w-full grid grid-cols-3 gap-8">
+
+        </div>
+      </div>
+    </div>
+  );
+};
