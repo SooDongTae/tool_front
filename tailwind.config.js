b9dfd434869e25cc393c2991ab0bde24ee9ef3bc
dohi0512 <donghuni512@gmail.com>
2023-04-04 11:44:16 +0900

    Add::Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
     colors: {
       "BlueLight-20": "#E1F5FE",
       "GreenLight-30": "#20C997",
+      "GrayScale-30": "#AEAEB2",
+      "GrayScale-20": "#C6C6C6",
+      "GrayScale-10": "#F4F4F4",
     },
   },
   plugins: [],
