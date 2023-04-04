bff5e56c26d1f620ee6ea13a7a71385b6ccc6d15
dohi0512 <donghuni512@gmail.com>
2023-04-04 11:44:45 +0900

    Add::헤더 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import { Header } from "@/components/header";
+import { Header } from "@/components/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <Header /> */}
+      <Header/>
       <Component {...pageProps} />
     </>
   );
