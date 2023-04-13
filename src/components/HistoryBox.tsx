e0e144fc483351c68d6e3c96321adedf48fce489
LLTae2 <taehyun5820@gmail.com>
2023-04-13 23:57:16 +0900

    Refactor::history box 부분 컴포넌트로 분리

---
+++ src/components/HistoryBox.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HistoryBox = () => {
+  return (
+    <div className="history-box">
+      <div className="w-[16%] history-content">2022-01-12</div>
+      <div className="w-[28%] history-content">초코에몽 공구</div>
+      <div className="w-[14%] history-content">12500원</div>
+      <div className="w-[14%] history-content">12500원</div>
+      <div className="w-[14%] history-content">0원</div>
+      <div className="w-[14%] text-center text-[1.3rem]">완료</div>
+    </div>
+  );
+};
+
+export default HistoryBox;
