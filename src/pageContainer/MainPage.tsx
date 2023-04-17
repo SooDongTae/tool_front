77c82530e217c441c3fed2257b00cd4439835147
dohi0512 <donghuni512@gmail.com>
2023-04-18 08:31:19 +0900

    Feat::카테고리 모달 추가

--- src/pageContainer/MainPage.tsx
+++ src/pageContainer/MainPage.tsx
@@ -2,6 +2,9 @@ import { Party } from "@/components/Party";
 import { SearchBar } from "@/components/SearchBar";
 import { PartyProps } from "@/types/Party.type";
 import Link from "next/link";
+import { AiFillCaretDown } from "react-icons/ai";
+import { useState } from "react";
+import { CategoryModal } from "@/components/CategoryModal";
 const TestData: PartyProps = {
   title: "파티 제목을 적어",
   maxPeople: 13,
@@ -9,13 +12,30 @@ const TestData: PartyProps = {
   price: 50000,
   endDate: "2023.04.01",
 };
+const Category = [
+  "전체",
+  "카테고리 1",
+  "카테고리 2",
+  "카테고리 3",
+  "카테고리 4",
+  "카테고리 5",
+];
 export const MainPage = () => {
+  const [open, setOpen] = useState(false);
+  const [category, setCategory] = useState(0);
+  const [title, setTitle] = useState("");
   return (
     <div className="w-screen h-screen flex justify-center pt-[8.5rem]">
       <div className="w-[75rem]">
         <div className="w-full h-[18rem] rounded-[2rem] bg-BlueLight-20"></div>
         <div className="w-full h-[2.5rem] flex justify-between mt-8 items-center">
-          <SearchBar />
+          <CategoryModal
+            setData={setCategory}
+            data={category}
+            category={Category}
+          />
+
+          <SearchBar setData={setTitle} />
           <Link href="/create">
             <div className="button-layout text-GreenLight-30 hover:bg-GreenLight-30 bg-[white] hover:text-[white] border-[.5px] border-[GreenLight-30]">
               파티 만들기
