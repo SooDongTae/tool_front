990c7f55fb1bd1bfbb13e1edfa24178e12b1d671
LLTae2 <taehyun5820@gmail.com>
2023-04-18 11:17:50 +0900

    Update::프로필 정보 박스 업데이트

--- src/pageContainer/ProfilePage.tsx
+++ src/pageContainer/ProfilePage.tsx
@@ -1,23 +1,71 @@
+import Image from "next/image";
 import React from "react";
+import ImgSrc from "../../public/12.png";
+import { SlScreenSmartphone } from "react-icons/sl";
+import { TfiEmail } from "react-icons/tfi";
 
 const ProfilePage = () => {
   return (
-    <div className="w-screen h-screen flex flex-col items-center">
-      <div className="w-[75rem] h-full flex flex-col justify-start mt-[12rem]">
-        <h2 className="text-[2rem] font-semibold">
-          <span className="text-GreenLight-30">My</span> 프로필
+    <div className="w-screen h-screen flex flex-row">
+      <div className="w-[35%] h-full flex flex-col items-center justify-start shadow-2xl shadow-[#777] pt-[3%] z-10">
+        <h2 className="text-[2.5rem] font-semibold">
+          <span className="text-GreenLight-30">My</span> 페이지
         </h2>
-        <div className="w-full h-[60%] mt-[2rem] flex flex-row justify-between">
-          <div className="w-[47%] h-full border-2">
-            <ul className="w-full h-full list-disc ml-[15%] text-[1.3rem] flex flex-col justify-center items-start">
-              <li className="m-[1%]">아이디 </li>
-              <li className="m-[1%]">연락처</li>
-              <li className="m-[1%]">이메일</li>
-              <li className="m-[1%]">가입일</li>
-            </ul>
-          </div>
-          <div className="w-[47%] h-full border-2"></div>
+        <div className="flex flex-col items-center justify-evenly mt-[12%]">
+          <Image
+            className="rounded-full"
+            width={200}
+            height={200}
+            src={ImgSrc}
+            alt="프로필 사진"
+          />
+          <span className="text-[2rem]">태현</span>
+          <span className="text-[1.3rem] text-[#777]">
+            taehyun5820@gmail.com
+          </span>
         </div>
+        <div className="mt-[12%]">
+          <div className="text-[2.3rem] border-b-[0.2rem] border-GreenLight-30">
+            내 프로필
+          </div>
+          <div className="text-[2.3rem] mt-[12%]">결제 내역</div>
+        </div>
+      </div>
+      <div className="w-[65%] h-full flex flex-col justify-evenly items-center bg-[#fafafa] ">
+        <div className="profile-box p-[4%]">
+          <div className="flex flex-row items-center border-b-[0.1rem] border-b-GrayScale-20 pb-[2%]">
+            <Image
+              className="rounded-full"
+              width={80}
+              height={80}
+              src={ImgSrc}
+              alt="프로필 사진"
+            />
+            <div className="flex flex-col ml-[5%]">
+              <span className="text-[1.5rem] font-semibold">태현</span>
+              <span className="text-[1rem] text-[#777]">
+                taehyun5820@gmail.com
+              </span>
+            </div>
+          </div>
+          <div className="profile-flex-box border-b-[0.1rem] border-GrayScale-20">
+            <div className="profile-info-box">
+              <SlScreenSmartphone size={35} />
+              <span className="text-[1.2rem] ml-[4%]">010-5820-7262</span>
+            </div>
+            <div className="profile-modify-btn">수정</div>
+          </div>
+          <div className="profile-flex-box">
+            <div className="profile-info-box">
+              <TfiEmail size={33} />
+              <span className="text-[1.2rem] ml-[4%]">
+                taehyun5820@gmail.com
+              </span>
+            </div>
+            <div className="profile-modify-btn">수정</div>
+          </div>
+        </div>
+        <div className="profile-box"></div>
       </div>
     </div>
   );
