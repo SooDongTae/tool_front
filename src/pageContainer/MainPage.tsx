1169ec7d93adbad1bdd2bd7d880805121d54fc8a
dohi0512 <donghuni512@gmail.com>
2023-04-19 23:23:50 +0900

    Style::아래쪽 여백 추가

--- src/pageContainer/MainPage.tsx
+++ src/pageContainer/MainPage.tsx
@@ -29,20 +29,21 @@ export const MainPage = () => {
       <div className="w-[75rem]">
         <div className="w-full h-[18rem] rounded-[2rem] bg-BlueLight-20"></div>
         <div className="w-full h-[2.5rem] flex justify-between mt-8 items-center">
-          <CategoryModal
-            setData={setCategory}
-            data={category}
-            category={Category}
-          />
-
-          <SearchBar setData={setTitle} />
+          <div className="flex flex-row items-center">
+            <CategoryModal
+              setData={setCategory}
+              data={category}
+              category={Category}
+            />
+            <SearchBar setData={setTitle} />
+          </div>
           <Link href="/create">
             <div className="button-layout text-GreenLight-30 hover:bg-GreenLight-30 bg-[white] hover:text-[white] border-[.5px] border-[GreenLight-30]">
               파티 만들기
             </div>
           </Link>
         </div>
-        <div className="w-full lg:grid-cols-3 md:grid-cols-2 grid grid-cols-1 gap-8 mt-[2rem] gap-y-[4rem]">
+        <div className="w-full lg:grid-cols-3 md:grid-cols-2 grid grid-cols-1 gap-8 mt-[2rem] gap-y-[4rem] pb-[8rem]">
           <Party {...TestData} />
           <Party {...TestData} />
           <Party {...TestData} />
