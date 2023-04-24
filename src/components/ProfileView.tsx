39bc74b08d3d6417512fb03f2a1c434e24c59406
LLTae2 <taehyun5820@gmail.com>
2023-04-24 19:15:07 +0900

    Update::history 페이지 디자인 수정, profile view에 탭 링크 걸었음

--- src/components/ProfileView.tsx
+++ src/components/ProfileView.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImgSrc from "../../public/12.png";
+import Link from "next/link";
 
 const ProfileView = () => {
+  const [isClient, setIsClient] = useState(false);
+  const path = window.location.pathname;
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
   return (
-    <div className="w-[30%] h-[calc(100vh-6rem)] mt-[9rem] flex flex-col items-center justify-start">
+    <div className="w-[30%] h-[calc(100vh-6rem)] pt-[9rem] flex flex-col items-center justify-start">
       <h2 className="text-[2.5rem] font-semibold">
         <span className="text-GreenLight-30">My</span> 페이지
       </h2>
@@ -19,11 +25,25 @@ const ProfileView = () => {
         <span className="text-[2rem]">태현</span>
         <span className="text-[1.3rem] text-[#777]">taehyun5820@gmail.com</span>
       </div>
-      <div className="mt-[12%]">
-        <div className="text-[2.3rem] border-b-[0.2rem] border-GreenLight-30">
-          내 프로필
+      <div className="mt-[12%] flex flex-col">
+        <div
+          className={`text-[2.3rem] box-border ${
+            isClient &&
+            path === "/profile" &&
+            "border-GreenLight-30 border-b-[0.2rem]"
+          }`}
+        >
+          <Link href={"/profile"}>내 프로필</Link>
         </div>
-        <div className="text-[2.3rem] mt-[12%]">결제 내역</div>
+        <div
+          className={`text-[2.3rem] mt-[12%] box-border ${
+            isClient &&
+            path === "/history" &&
+            "border-GreenLight-30 border-b-[0.2rem]"
+          }`}
+        >
+          <Link href={"/history"}>결제 내역</Link>
+        </div>
       </div>
     </div>
   );
--- src/pageContainer/HistoryPage.tsx
+++ src/pageContainer/HistoryPage.tsx
@@ -6,18 +6,19 @@ const HistoryPage = () => {
   return (
     <div className="w-screen h-screen flex flex-row justify-between">
       <ProfileView />
-      <div className="w-[65%] h-screen flex flex-col items-center">
-        <div className="w-full h-full flex flex-col items-center justify-start mt-[12rem]">
-          <h2 className="text-[2rem] font-semibold">
+      <div className="w-[70%] h-screen flex flex-col items-center bg-[#fafafa] overflow-scroll">
+        <div className="w-full h-full flex flex-col items-center justify-start mt-[10rem]">
+          <h2 className="w-[65%] text-[2rem] font-semibold text-left">
             <span className="text-GreenLight-30">My</span> 결제 내역
           </h2>
-          <div className="w-[65%] mt-[2rem] ">
+          <div className="w-[70%] h-full mt-[2rem]">
             <div className="w-full h-[5rem] bg-GrayScale-15 border-r-0 border-l-0 border-b-0 border-t-GreenLight-30 border-[0.3rem] flex flex-row items-center">
-              <div className="w-[16%] history-content">진행날짜</div>
-              <div className="w-[28%] history-content">서비스명</div>
-              <div className="w-[14%] history-content">결제 금액</div>
-              <div className="w-[14%] text-center text-[1.3rem]">현재 상태</div>
+              <div className="w-[20%] history-content">진행날짜</div>
+              <div className="w-[40%] history-content">서비스명</div>
+              <div className="w-[20%] history-content">결제 금액</div>
+              <div className="w-[20%] text-center text-[1.3rem]">현재 상태</div>
             </div>
+            <HistoryBox />
             <HistoryBox />
             <HistoryBox />
             <HistoryBox />
