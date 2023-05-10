315b3716ac47ee320d381994351ef464f311c8bc
dohi0512 <donghuni512@gmail.com>
2023-05-10 20:20:29 +0900

    Fix:: 헤더 삭제

--- src/api/party/getPartyList.ts
+++ src/api/party/getPartyList.ts
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-export const GetPartyList = async () => {
+export const GetAllPartyList = async () => {
   try {
     const response = await axios.get(
-      `/api/groupBuying/list?limit=${5}&offset=${0}&sortField=${"views"}&sortWay=${"desc"}&category=${"all"}&title=${""}&status=${"ACTIVATED"}`,
-      {
-        headers: {
-          Authorization: `Bearer${localStorage.getItem("accessToken")}`,
-        },
-      }
+      `/api/groupBuying/list?limit=${5}&offset=${0}&sortField=${"views"}&sortWay=${"desc"}&category=${"all"}&title=${""}&status=${"ACTIVATED"}`
     );
     return response.data;
   } catch (e) {
