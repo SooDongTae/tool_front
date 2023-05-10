ac668812943b270b8fae4919b0402949de9a8094
dohi0512 <donghuni512@gmail.com>
2023-05-10 13:51:44 +0900

    React Query Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,20 @@
 import { Header } from "@/components/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ToastContainer } from "react-toastify";
-
 import "react-toastify/dist/ReactToastify.css";
 import { useState } from "react";
-
+import { RecoilRoot } from "recoil";
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
         <Header />
         <Component {...pageProps} />
         <ToastContainer limit={1} position="bottom-right" />
-      </Hydrate>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
