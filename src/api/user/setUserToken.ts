f0af7156fb610696fb9ca1329ea806ddb36f4238
dohi0512 <donghuni512@gmail.com>
2023-05-11 11:14:35 +0900

    Update:: 유저 토큰 저장

--- src/api/user/setUserToken.ts
+++ src/api/user/setUserToken.ts
@@ -4,7 +4,10 @@ export const SetUserToken = async (code: string) => {
   const headers = { authCode: code };
   try {
     const { data } = await axios.post("/api/auth/oauth/bsm", null, { headers });
+    console.log(data);
     localStorage.setItem("accessToken", data.accessToken);
+    localStorage.setItem("refreshToken", data.refreshToken);
+    localStorage.setItem("expire_at", data.expire_at);
   } catch (e) {
     console.log(e);
   }
