913f7bf5ecee475a915349b5151fffc7d7a29dcc
dohi0512 <donghuni512@gmail.com>
2023-05-16 16:39:54 +0900

    Fix::유저 정보 받아오기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,13 @@
-import UserAPI from "@/api/User";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { HeaderText } from "./HeaderText";
-import { useQuery } from "react-query";
-import { useMemo } from "react";
+import { useQueryClient } from "react-query";
+import { useState } from "react";
+import useUser from "@/hooks/useUser";
 export const Header = () => {
-  const router = useRouter();
-  const userQuery = useQuery(
-    ["user"],
-    () => UserAPI.GetUserName(localStorage.getItem("accessToken"))
-    // { enabled:  !!localStorage.getItem("accessToken") }
-  );
-  const userName = useMemo(
-    () =>
-      userQuery.data?.grade.toString() +
-        userQuery.data?.classNo.toString() +
-        (userQuery.data?.stuNo < 10 ? "0" : "") +
-        userQuery.data?.stuNo.toString() +
-        userQuery.data?.name || "",
-    [userQuery]
-  );
+  const [clicked, setClicked] = useState(false);
+  const queryClinet = useQueryClient();
+  const { isLogged, user, logout } = useUser();
+  console.log(user);
   return (
     <div className="w-full h-[6rem] fixed shadow-md bg-[white] z-50 flex justify-center items-center">
       <div className="w-[75rem]  flex flex-row items-center justify-between">
@@ -29,10 +17,45 @@ export const Header = () => {
           </Link>
           <HeaderText target="/" text="공동구매" />
           <HeaderText target="/community" text="커뮤니티" />
-          {/* <HeaderText target="" text="중고거래" /> */}
         </div>
-        {userName ? (
-          <HeaderText target="" text={userName} />
+        {isLogged ? (
+          <div className="relative">
+            <div onClick={() => setClicked((prev) => !prev)}>
+              <HeaderText
+                target=""
+                text={
+                  user?.grade.toString() +
+                  user?.classNo.toString() +
+                  (user?.stuNo < 10 ? "0" : "") +
+                  user?.stuNo.toString() +
+                  " " +
+                  user?.name
+                }
+                ml="0"
+              />
+            </div>
+            {clicked ? (
+              <div className="absolute top-full left-1/2 translate-x-[-50%] w-[8rem] flex-col shadow-[rgba(0,_0,_0,_0.08)_0px_0px_4px] rounded-[10px] bg-white cursor-pointer">
+                <div
+                  className="h-[3.5rem] flex justify-center items-center border-b-[1px] border-b-GrayScale-15"
+                  onClick={() => setClicked(false)}
+              
+                >
+                  <HeaderText text="마이페이지" target="/profile" ml="0" />
+                </div>
+                <div
+                  className="h-[3.5rem] flex justify-center items-center"
+                  onClick={() => {
+                    setClicked(false);
+                    logout();
+                    queryClinet.invalidateQueries(["user"]);
+                  }}
+                >
+                  <HeaderText text="로그아웃" target="" ml="0" />
+                </div>
+              </div>
+            ) : null}
+          </div>
         ) : (
           <HeaderText
             target="https://auth.bssm.kro.kr/oauth?clientId=98fd44ad&redirectURI=http://localhost:3000/oauth"
