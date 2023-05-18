3957f64123c897a06235f0d1164f82812075d5b4
dohi0512 <donghuni512@gmail.com>
2023-05-18 17:01:40 +0900

    Update:: 파티에 이미지 추가

--- src/components/Party.tsx
+++ src/components/Party.tsx
@@ -10,34 +10,38 @@ export const Party = ({
   grade,
   studentNum,
   classNum,
+  imgSrc,
 }: PartyProps) => {
   return (
-    <div className="w-full h-[16rem] rounded-[10px] border-[1px] border-GrayScale-20 flex justify-center items-center shadow-[rgba(0,_0,_0,_0.1)_0px_4px_16px_0px] hover:shadow-[rgba(0,_0,_0,_0.2)_0px_7px_16px_0px] hover:translate-y-[-.6rem] duration-300 cursor-pointer hover:border-GreenLight-30 ">
-      <div className="w-[85%] h-[90%] flex flex-col">
-        <div className="text-3xl font-semibold w-full text-omit h-[2.5rem]">
-          {title}
+    <div className="bg-white w-full h-[22rem] rounded-[10px]  border-GrayScale-20  shadow-[rgba(0,_0,_0,_0.1)_0px_4px_16px_0px] hover:shadow-[rgba(0,_0,_0,_0.2)_0px_7px_16px_0px] hover:translate-y-[-.6rem] duration-300 cursor-pointer  flex items-center flex-col">
+      <img
+        src={imgSrc.substring(21)}
+        className="w-full h-[12rem] rounded-t-[9px] object-cover"
+      />
+      <div className="w-[80%] flex flex-col">
+        <div className="flex ">
+          <div className="font-semibold text-2xl mt-2">{title}</div>
         </div>
-        <span className="text-xs font-bold text-GrayScale-20 h-[7rem]">
+        <div className="text-xs text-GrayScale-40 ">
           {grade}
           {classNum}
-          {studentNum < 10 ? "0" : null}
+          {studentNum < 10 ? 0 : null}
           {studentNum} {owner}
-        </span>
-        <ProgressBar
-          width="full"
-          height="1rem"
-          maxi={maxPeople}
-          current={currentPeople}
-          color={"GreenLight-30"}
-        />
-        <span className="text-sm  font-semibold h-[2rem]">{`${currentPeople}/${maxPeople} (${Math.floor(
-          (currentPeople / maxPeople) * 100
-        )}%)`}</span>
-        <div className="flex flex-row justify-between mt-[0.5rem]">
-          <span className="text-xs text-GrayScale-30 font-bold">
-            {endDate.substring(0, 10)}
+        </div>
+        <div className="mt-6">
+          <span className="text-sm">
+            {(currentPeople / maxPeople) * 100}% ({currentPeople}/{maxPeople})
           </span>
-          <span className=" text-lg font-bold">{price}원</span>
+          <ProgressBar
+            maxi={maxPeople}
+            current={currentPeople}
+            width="full"
+            height="1rem"
+            color="GreenLight-30"
+          />
+        </div>
+        <div className="text-GreenLight-30 font-semibold mt-2">
+          {price.toLocaleString()}원
         </div>
       </div>
     </div>
