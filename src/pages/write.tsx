bcff4d051f539f5600733fe02c8a8a77ff659e07
이동훈 <202110309@bssm.hs.kr>
2023-06-08 00:48:50 +0900

    Fix:: 로그인 안 되있으면 로그인 페이지로 넘어감

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,6 +1,19 @@
+import { userState } from "@/context/userState";
 import { WritePage } from "@/pageContainer/WritePage";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
 
 export const Write = () => {
+  const [user, setUser] = useRecoilState(userState);
+  const router = useRouter();
+  useEffect(() => {
+    if (!!user.id === false) {
+      router.push(
+        "https://auth.bssm.kro.kr/oauth?clientId=98fd44ad&redirectURI=http://localhost:3000/oauth"
+      );
+    }
+  });
   return <WritePage />;
 };
 
