087634c9af1a5dbebe2ddeec962b4bafeea6c893
LLTae2 <taehyun5820@gmail.com>
2023-06-12 11:38:52 +0900

    Update::요청부분 컴포넌트로 분리

--- src/pageContainer/RequestPage.tsx
+++ src/pageContainer/RequestPage.tsx
@@ -1,4 +1,5 @@
-import useRequest from "@/hooks/useRequest";
+import Request from "@/components/Request";
+import useRequest from "@/hooks/request/useRequest";
 import { IRequest, IRequestList } from "@/types/Request.type";
 import React from "react";
 
@@ -19,34 +20,9 @@ const RequestPage = () => {
           </h2>
         </div>
         <div className="w-[70%] flex flex-row flex-wrap justify-between mt-[2rem]">
-          <div className="w-[48%] h-[5rem] shadow-[0_0_10px_0_rgba(0,0,0,0.3)] rounded-[15px] mb-[1rem] flex flex-row justify-evenly items-center">
-            <img
-              className="rounded-full w-[3rem] h-[3rem]"
-              src="https://avatars.githubusercontent.com/u/80657114?s=40&v=4"
-              alt="프로필 사진"
-            />
-            <p className="text-[1.3rem] w-[50%] font-semibold">3111 이태현</p>
-            <div className="w-[4rem] h-[2rem] rounded-full bg-GreenLight-30 text-center leading-[2rem] text-white">
-              수락
-            </div>
-            <div className="w-[4rem] h-[2rem] rounded-full bg-GrayScale-20 text-center leading-[2rem] text-white font-semibold">
-              거절
-            </div>
-          </div>
-          <div className="w-[48%] h-[5rem] shadow-[0_0_10px_0_rgba(0,0,0,0.3)] rounded-[15px] mb-[1rem] flex flex-row justify-evenly items-center">
-            <img
-              className="rounded-full w-[3rem] h-[3rem]"
-              src="https://avatars.githubusercontent.com/u/80657114?s=40&v=4"
-              alt="프로필 사진"
-            />
-            <p className="text-[1.3rem] w-[50%] font-semibold">3111 이태현</p>
-            <div className="w-[4rem] h-[2rem] rounded-full bg-GreenLight-30 text-center leading-[2rem] text-white">
-              수락
-            </div>
-            <div className="w-[4rem] h-[2rem] rounded-full bg-GrayScale-20 text-center leading-[2rem] text-white font-semibold">
-              거절
-            </div>
-          </div>
+          {requestList?.map((request) => (
+            <Request {...request} />
+          ))}
         </div>
       </div>
     </div>
