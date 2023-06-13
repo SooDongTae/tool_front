abde2baee8a3cd95760943d469cdae848517727a
이동훈 <202110309@bssm.hs.kr>
2023-06-13 22:09:25 +0900

    Update:: 타입 수정

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -1,10 +1,14 @@
 import { DetailPage } from "@/pageContainer/DetailPage";
 import { NextPageContext } from "next";
-export const Detail = ({ id }: { id: string | undefined | string[] }) => {
+export const Detail = ({ id }: { id: string }) => {
+  console.log(id);
   return <DetailPage id={id} />;
 };
 export const getServerSideProps = async (ctx: NextPageContext) => {
   const { id } = ctx.query;
-  return { props: { id } };
+  if (typeof id === "string") {
+    return { props: { id } };
+  }
+  return { props: { id: "0" } };
 };
 export default Detail;
