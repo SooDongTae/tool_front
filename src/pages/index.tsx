ff31a0c877f352cc8fd3cbb2c7b11b54f74221b9
dohi0512 <donghuni512@gmail.com>
2023-06-19 22:22:03 +0900

    Fix:: 새로고침시 유저 정보 유지

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { NextSeo } from "next-seo";
 import { MainPage } from "@/pageContainer/MainPage";
 import useConfig from "@/hooks/useConfig";
-
 export default function Home() {
   const { seoConfig } = useConfig({
     title: "Tool",
