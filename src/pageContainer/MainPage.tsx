87577d6de659eec7f08b3ef802e5240028f161cc
dohi0512 <donghuni512@gmail.com>
2023-06-19 22:21:42 +0900

    Update:: 챗 보내는 방식 수정

--- src/pageContainer/MainPage.tsx
+++ src/pageContainer/MainPage.tsx
@@ -11,10 +11,8 @@ import { SortField } from "@/components/Main/SortField";
 import { Loading } from "@/components/Shared/Loading";
 import { ChatBot } from "@/components/Shared/ChatBot";
 import { IParty } from "@/types/Party.type";
-import Image from "next/image";
 const category = ["all", "PRODUCT", "FOOD", "CLOTHES", "ETC"];
 export const MainPage = () => {
-  const [open, setOpen] = useState(false);
   const [cateIdx, setCateIdx] = useState(0);
   const [title, setTitle] = useState("");
   const [sortField, setSortField] = useState("views");
@@ -24,9 +22,7 @@ export const MainPage = () => {
     sortField: sortField,
   });
   const handleIntersection = () => {
-    if (hasNextPage) {
-      fetchNextPage();
-    }
+    if (hasNextPage) fetchNextPage();
   };
   const party = useMemo(
     () =>
@@ -42,14 +38,14 @@ export const MainPage = () => {
   });
   return (
     <div className="layout">
-      <div className="lg:w-[75rem] w-[80%]">
+      <div className="container">
         <div className="h-[18rem] rounded-[10px] bg-BlueLight-20 text-4xl font-bold text-GreenLight-30 flex justify-center items-center">
           <a href="https://tool-landing-page.vercel.app" target="_blink">
             Tool 사용법이 궁금하다면?
           </a>
         </div>
         <div className="lg:h-[2.5rem] h-[7rem] flex justify-between mt-8 lg:items-center lg:flex-row flex-col">
-          <div className="flex flex-row items-center justify-between lg:w-[20rem]">
+          <div className="flex items-center justify-between lg:w-[20rem]">
             <CategoryModal
               setData={setCateIdx}
               data={cateIdx}
@@ -70,7 +66,7 @@ export const MainPage = () => {
               text="최신순"
             />
           </div>
-          <div className="flex flex-row justify-between items-center">
+          <div className="flex justify-between items-center">
             <SearchBar setData={setTitle} />
             <Link
               href="/create"
@@ -85,25 +81,7 @@ export const MainPage = () => {
         </div>
       </div>
       {hasNextPage && <Observer handleIntersection={handleIntersection} />}
-      <div
-        className="fixed bg-white right-10 bottom-10 w-[4rem] h-[4rem] shadow-[rgba(0,_0,_0,_0.1)_0px_4px_16px_0px] hover:border-GreenLight-30 border-[.5px] border-GrayScale-15 cursor-pointer  duration-300 rounded-[25px] flex justify-center items-center"
-        onClick={() => setOpen((prev) => !prev)}
-      >
-        {open ? (
-          <>
-            <ChatBot />
-            <AiOutlineClose size={"3rem"} className="text-GreenLight-30" />
-          </>
-        ) : (
-          <Image
-            src="/chat.png"
-            width={100}
-            height={100}
-            priority={true}
-            alt="챗봇 이미지"
-          />
-        )}
-      </div>
+      <ChatBot />
     </div>
   );
 };
